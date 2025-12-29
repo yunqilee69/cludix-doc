2cab53fd5f8b1bba2104312b97360b8b4493c6c1
YunqiLee69 <yunqilee69@gmail.com>
2025-12-29 22:08:49 +0800

    ft: 调整sidebars的配置

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
       "classic",
       {
         docs: {
-          sidebarPath: "./sidebars.ts",
+          sidebarPath: "./sidebars",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
